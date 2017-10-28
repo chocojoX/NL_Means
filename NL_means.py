220438ef195242afd1b8389092b96aacda2f9845
chocojoX <joseph.budin@polytechnique.edu>
2017-10-28 09:02:46 +0200

    Created base files

---
+++ NL_means.py
@@ -0,0 +1,9 @@
+from __future__ import division
+
+import numpy as np
+import scipy as scp
+import pylab as pyl
+import matplotlib.pyplot as plt
+
+from nt_toolbox.general import *
+from nt_toolbox.signal import *
