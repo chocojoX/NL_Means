286b01990a975b3a834b5ac78e8b98f40824091f
chocojoX <joseph.budin@polytechnique.edu>
2017-10-28 09:54:56 +0200

    Added black and white local PCA descriptor

--- NL_means.py
+++ NL_means.py
@@ -4,24 +4,72 @@ import numpy as np
 import scipy as scp
 import pylab as pyl
 import matplotlib.pyplot as plt
+from numpy import linalg
 
 from NL_tools import *
 from nt_toolbox.general import *
 from nt_toolbox.signal import *
 
 
-def preprocess():
+def preprocess(sigma):
     n = 128
     c = [100,200]
     f0 = load_image("nt_toolbox/data/lena.bmp")
 
     f0 = rescale(f0[c[0]-n//2:c[0]+n//2, c[1]-n//2:c[1]+n//2])
-    y = add_noise(0.04, f0)
+    y = add_noise(sigma, f0)
 
     return f0, y
 
 
+class NL_Means(object):
+    def __init__(self, sigma=0.04, color=False, w=3):
+        self.sigma = sigma
+
+        self.color = False
+        f0, y = preprocess(self.sigma)
+        self.f0 = f0
+        self.y = y
+        self.n = f0.shape[0]
+
+        """ nl-means params"""
+        self.w = 3
+        self.w1 = 2*self.w + 1
+        self.pca_dim = 25
+
+
+    def create_patches(self, pict):
+        patches = extract_all_patches(self.w, pict)
+        self.patches = patches
+
+
+    def compute_PCA_patches(self):
+        flat_patches = patches_to_2D(self.patches)
+        flat_patches = flat_patches - np.tile(np.mean(flat_patches,0),(self.w1**2, 1))
+
+        C = np.dot(flat_patches, np.transpose(flat_patches))
+        [D,V] = linalg.eig(C)
+        self.D = np.sort(D)[::-1]   # Diagonal matrix containing the eigen values of C
+        I = np.argsort(D)[::-1]
+        self.V = V[I,:]             # Ortogonal matrix of diagonalization of C
+
+        Q = np.dot(np.transpose(self.V[: ,:self.pca_dim]), flat_patches)
+        self.H = np.reshape(np.transpose(Q),(self.n, self.n, self.pca_dim),order="F")   #Descriptor of patches (n*n*pca_dim)
+
+
+
 
 if __name__ == "__main__":
-    f0, y = preprocess()
-    plot_picture(y)
+    nl = NL_Means()
+    plot_picture(nl.y)
+
+    plt.figure(figsize = (5,5))
+
+    nl.create_patches(nl.y)
+    P = nl.patches
+    n = 128
+    for i in range(16):
+        x = np.random.randint(n)
+        y = np.random.randint(n)
+        imageplot(P[x, y], '', [4, 4, i+1])
+    plt.show()
--- NL_tools.py
+++ NL_tools.py
@@ -19,3 +19,27 @@ def add_noise(sigma, f0):
     n = f0.shape[0]
     f = f0 + sigma*np.random.standard_normal((n,n))
     return f
+
+
+def extract_all_patches(w, pict):
+    n = pict.shape[0]
+    [X,Y,dX,dY]=np.meshgrid(np.arange(1,n+1),np.arange(1,n+1),np.arange(-w,w+1),np.arange(-w,w+1))
+    X = X + dX
+    Y = Y + dY
+
+    X[X < 1] = 2-X[X < 1]
+    Y[Y < 1] = 2-Y[Y < 1]
+    X[X > n] = 2*n-X[X > n]
+    Y[Y > n] = 2*n-Y[Y > n]
+
+    I = (X-1) + (Y-1)*n
+    for i in range(n//w):
+        for j in range(n//w):
+            I[i,j] = np.transpose(I[i,j])
+
+    patch = np.ravel(pict)[I]
+    return patch
+
+
+def patches_to_2D(patches):
+    return np.transpose((np.reshape(patches, (n*n,w1*w1), order="F")))
